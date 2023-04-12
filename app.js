2f94a3c3878a323558904acfe94909a5bf9a86c7
OmarHamada1996 <omer.jasser@gmail.com>
2023-04-12 19:47:36 +0000

    adding quistions

--- app.js
+++ app.js
@@ -16,3 +16,35 @@ if (gender === "male") {
 if (!skipWelcoming) {
   alert("Welcome, " + greeting + "!");
 }
+
+let answers = [];
+
+function askQuestion(question) {
+  let answer = prompt(question);
+  if (answer === null || answer.trim() === '') {
+    answer = 'invalid';
+  }
+  return answer.toLowerCase();
+}
+
+function addAnswer(answer) {
+  answers.push(answer);
+}
+
+function printAnswers() {
+  console.log(answers);
+}
+
+let question1 = 'Do you have any experiance in programming ?';
+let answer1 = askQuestion(question1);
+addAnswer(answer1);
+
+let question2 = 'Have you ever took any programing courses ';
+let answer2 = askQuestion(question2);
+addAnswer(answer2);
+
+let question3 = 'Do you enjoy this course?';
+let answer3 = askQuestion(question3);
+addAnswer(answer3);
+
+printAnswers();
